57a223e9359c3e86fbff0a5913e57c01503ca088
Muhamad Arief <muhamadarief3101@gmail.com>
2021-08-15 06:37:24 +0700

    upgrade grade & android studio arctic fox

--- app/src/main/java/id/aibangstudio/basekotlin/di/Modules.kt
+++ app/src/main/java/id/aibangstudio/basekotlin/di/Modules.kt
@@ -1,26 +1,24 @@
 package id.aibangstudio.basekotlin.di
 
 import androidx.room.Room
+import id.aibangstudio.basekotlin.data.db.AppDatabase
+import id.aibangstudio.basekotlin.data.pref.PreferencesHelper
+import id.aibangstudio.basekotlin.data.remote.createWebService
+import id.aibangstudio.basekotlin.data.remote.provideOkHttpClient
+import id.aibangstudio.basekotlin.data.remote.service.TeamService
+import id.aibangstudio.basekotlin.data.repository.TeamRepository
+import id.aibangstudio.basekotlin.data.repository.TeamRepositoryImpl
+import id.aibangstudio.basekotlin.presentation.main.MainViewModel
+import id.aibangstudio.momakan.utils.scheduler.AppSchedulerProvider
+import id.aibangstudio.momakan.utils.scheduler.SchedulerProvider
 import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import id.aibangstudio.basekotlin.data.db.AppDatabase
-import id.aibangstudio.basekotlin.data.remote.createWebService
-import id.aibangstudio.basekotlin.data.remote.provideOkHttpClient
-import id.aibangstudio.basekotlin.data.pref.PreferencesHelper
-import id.aibangstudio.basekotlin.data.remote.service.MusicService
-import id.aibangstudio.basekotlin.data.repository.TeamRepository
-import id.aibangstudio.basekotlin.data.repository.TeamRepositoryImpl
-import id.aibangstudio.basekotlin.data.remote.service.TeamService
-import id.aibangstudio.basekotlin.presentation.main.MainViewModel
-import id.aibangstudio.momakan.utils.scheduler.AppSchedulerProvider
-import id.aibangstudio.momakan.utils.scheduler.SchedulerProvider
 
 val appModule = module {
     single { provideOkHttpClient() }
     single { createWebService<TeamService>(get()) }
-    single { createWebService<MusicService>(get()) }
 
     single { PreferencesHelper(androidContext()) }
 
